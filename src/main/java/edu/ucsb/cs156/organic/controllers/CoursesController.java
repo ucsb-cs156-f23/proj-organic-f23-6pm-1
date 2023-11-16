2724abeaf040e8e71c46407767b7e5e16f3a9f00
jakedel <42163241+jakedel@users.noreply.github.com>
2023-11-16 15:04:53 -0800

    add function in CoursesController to delete staff

--- src/main/java/edu/ucsb/cs156/organic/controllers/CoursesController.java
+++ src/main/java/edu/ucsb/cs156/organic/controllers/CoursesController.java
@@ -19,6 +19,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -115,6 +116,18 @@ public class CoursesController extends ApiController {
         log.info("courseStaff={}", courseStaff);
 
         return courseStaff;
+    }
+
+    @Operation(summary = "Remove staff from a course")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/staff")
+    public Object deleteStaff(
+            @Parameter(name = "id") @RequestParam Long id) {
+        Staff staff = courseStaffRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Staff.class, id.toString()));
+
+        courseStaffRepository.delete(staff);
+        return genericMessage("Staff with id %s deleted".formatted(id));
     }
 
     @Operation(summary = "Get Staff for course")
