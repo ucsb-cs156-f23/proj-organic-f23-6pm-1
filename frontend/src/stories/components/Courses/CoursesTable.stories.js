cc094b64a2d4be3be8a709a6760d28756e9e8d13
Dagin Gregory <dagin.gregory@gmail.com>
2023-11-21 22:14:30 -0800

    Addressed preivous FixMe issue

--- frontend/src/stories/components/Courses/CoursesTable.stories.js
+++ frontend/src/stories/components/Courses/CoursesTable.stories.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import CoursesTable from "main/components/Courses/CoursesTable";
 import { coursesFixtures } from 'fixtures/coursesFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+import { rest } from "msw";
+import CoursesTable from 'main/components/Courses/CoursesTable';
 
 export default {
     title: 'components/Courses/CoursesTable',
@@ -16,16 +18,27 @@ const Template = (args) => {
 export const Empty = Template.bind({});
 
 Empty.args = {
-    course: []
+    courses: []
 };
 
-export const OneCourse = Template.bind({});
+export const ThreeItemsOrdinaryUser = Template.bind({});
 
-OneCourse.args = {
-    course: coursesFixtures.oneCourse,
+ThreeItemsOrdinaryUser.args = {
+    courses: coursesFixtures.threeCourses,
+    currentUser: currentUserFixtures.userOnly,
 };
 
-export const ThreeCourses = Template.bind({});
-ThreeCourses.args = {
-    course: coursesFixtures.threeCourses,
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+    courses: coursesFixtures.threeCourses,
+    currentUser: currentUserFixtures.adminUser,
 }
+
+ThreeItemsAdminUser.parameters = {
+    msw: [
+        rest.delete('/api/courses', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200), ctx.json({}));
+        }),
+    ]
+};
