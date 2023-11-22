52524e6423234a12f47d02ba6055445880f878bb
Dagin Gregory <dagin.gregory@gmail.com>
2023-11-22 14:45:47 -0800

    Revert "Gave ROLE_INSTRUCTOR edit and delete permissions"
    This reverts commit 9e961c4fa786a74a205aa2005b76d922d6bf35e4.

--- frontend/src/main/components/Courses/CoursesTable.js
+++ frontend/src/main/components/Courses/CoursesTable.js
@@ -1,38 +1,42 @@
 import React from "react";
 import OurTable from "main/components/OurTable"
 
-const columns = [
-    {
-        Header: 'id',
-        accessor: 'id', // accessor is the "key" in the data
-    },
-    {
-        Header: 'Course Name',
-        accessor: 'name',
-    },
-    {
-        Header: 'School',
-        accessor: 'school',
-    },
-    {
-        Header: 'Term',
-        accessor: 'term',
-    },
-    {
-        Header: 'Start Date',
-        accessor: 'start',
-    },
-    {
-        Header: 'End Date',
-        accessor: 'end',
-    },
-    {
-        Header: 'GitHub Org',
-        accessor: 'ghOrg',
-    },
-];
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'Course Name',
+            accessor: 'name',
+        },
+        {
+            Header: 'School',
+            accessor: 'school',
+        },
+        {
+            Header: 'Term',
+            accessor: 'term',
+        },
+        {
+            Header: 'Start Date',
+            accessor: 'start',
+        },
+        {
+            Header: 'End Date',
+            accessor: 'end',
+        },
+        {
+            Header: 'GitHub Org',
+            accessor: 'githubOrg',
+        },
+    ];
 
-export default function CoursesTable({ courses }) {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        columns.push(ButtonColumn("Edit", "primary", editCallback, "CoursesTable"));
+        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "CoursesTable"));
+    }
+
     return <OurTable
         data={courses}
         columns={columns}
