32698c57adf1c45503bd8b0e4e2ef71ed90aee4a
Dagin Gregory <dagin.gregory@gmail.com>
2023-11-28 14:45:47 -0800

    Changed navBar for testing

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -48,15 +48,16 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <NavDropdown title="Admin" id="appnavbar-admin-dropdown" data-testid="appnavbar-admin-dropdown" >
                     <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
                     <NavDropdown.Item href="/admin/jobs">Manage Jobs</NavDropdown.Item>
-                    <NavDropdown.Item href="/admin/Courses">Manage Courses</NavDropdown.Item>
+                    <NavDropdown.Item href="/admin/courses">Manage Courses</NavDropdown.Item>
                   </NavDropdown>
-                )/*
-                hasRole(currentUser, "ROLE_ADMIN") && (
+                )
+              }
+              {
+                hasRole(currentUser, "ROLE_INSTRUCTOR") && (
                   <NavDropdown title="Admin" id="appnavbar-admin-dropdown" data-testid="appnavbar-admin-dropdown" >
-                    <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
-                    <NavDropdown.Item href="/admin/jobs">Manage Jobs</NavDropdown.Item>
+                    <NavDropdown.Item href="/admin/courses">Manage Courses</NavDropdown.Item>
                   </NavDropdown>
-                )*/
+                )
               }
             </Nav>
 
